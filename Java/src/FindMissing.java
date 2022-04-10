7d9671329199cb9771e802e1f23ca7a43758109c
Prabhu Kiran Konda <prabhukiran8790@protonmail.com>
2022-04-11 00:16:40 +0530

    Find Missing Number

---
+++ Java/src/FindMissing.java
@@ -0,0 +1,33 @@
+public class FindMissing {
+    static int solve(int[] nums) {
+        int sum = 0;
+        for (int e: nums) sum += e;
+        int n = nums.length;
+        return n*(n+1)/2 - sum;
+    }
+
+    static int solve2(int[] nums) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            int correct = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[correct]) {
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else i++;
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (nums[j] != j) return j;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 0};
+        System.out.println(solve(nums));
+        System.out.println(solve2(nums));
+    }
+}
