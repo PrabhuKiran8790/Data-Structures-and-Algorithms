6fce0e4d22011f819cb646551961c5f51d28e650
Prabhu Kiran Konda <prabhukiran8790@protonmail.com>
2022-04-10 22:52:55 +0530

    First and Last Index

--- Python/first_last_index.py
+++ Python/first_last_index.py
@@ -11,8 +11,14 @@ def solve(nums, target):
             start = mid + 1
         if nums[mid] == target:
             index[0] = mid
-    # resetting start and end
-    start, end = 0, len(nums)
+
+    # if index[0] is -1, it means, the target element is not found. so no need to search for last index
+    if index[0] == -1:
+        return index
+
+    # resetting start and end (start will be from the index of 1st occurrence)
+    start, end = index[0], len(nums)
+
     while start <= end:
         mid = start + (end - start) // 2
         if nums[mid] <= target:
