ea7a0a53ffeda5a340e9a749fcbca40959ac48fc
Prabhu Kiran Konda <prabhukiran8790@protonmail.com>
2022-04-12 13:51:41 +0530

    Contains Duplicates

---
+++ Java/src/ContainsDuplicates.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class ContainsDuplicates {
+    static boolean solve(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) return true;
+            seen.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(solve(nums));
+    }
+
+}
